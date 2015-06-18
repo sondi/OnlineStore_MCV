a83ff550cd1d5d08c7c7366c01846be0c8b6d0fa
codeamac01 <codeamac01@gmail.com>
2015-06-18 18:52:47 -0500

    Method Sign in

--- app/controller/main_controller.rb
+++ app/controller/main_controller.rb
@@ -3,24 +3,30 @@ class MainController
 		@view = MainView.new
 		# @usuario = "Sandra"
 		# @contra = "laquesea"
-		
-		log_in("Sandra", "nose")
+			if var[0] == "login"
+			log_in("Sandra", "nose")
+		elsif var[0] == "signin"
+			p var
+			sign_in(var[1],var[2])
+		else
+			puts "command not found"
+		end
 	end
 
-	def log_in(usuario, contra)
+	def log_in(username, password)
 		first_user = User.find(1)
 		username = first_user.name
 		pw = first_user.password
-
-		if contra == pw
+		if password == pw
 			puts "Welcome!"
 		else
 			puts "Username or Password not valid, try again"
 		end
 	end
 
-	def sign_in
-
+	def sign_in(username, password)
+		new_user = User.new(name: username, password: password)
+		new_user.save
 	end
 end
 
